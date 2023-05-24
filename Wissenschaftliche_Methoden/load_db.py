4f52fccccf1cd02cd41cb822bad3501e793a9930
1035 <hannover1992@googlemail.com>
2023-05-24 23:31:42 +0200

    st build in for nice look, split it to , creat db , appen ddb, and use db

--- Wissenschaftliche_Methoden/load_db.py
+++ Wissenschaftliche_Methoden/load_db.py
@@ -1,4 +1,5 @@
 import os
+import streamlit as st
 from global_var import chunk, overlap
 
 from langchain.embeddings.openai import OpenAIEmbeddings
@@ -19,13 +20,29 @@ query = "Wie Schreibt man en Abstract?"
 
 
 # prompt = st.text_input('Niebo gwazdziste nademna, prawo moralne we mnie. A ty, czym jesteś?Jestem twoją wolnością. Jestem tym, co masz, czego się trzymasz, czym możesz zdecydować i dążyć do tego, co uważasz za słuszne. Jestem tym, co możesz zmienić i wpłynąć na życie innych. Jestem tym, co jest w twojej ręce.')
-prompt = 'how ot wirte an abstract?'
+prompt = st.text_input('Niebo Moralne we mnie , niebo gwiazdziste nade mna , a ty czym jestes?')
 search = db.similarity_search_with_score(prompt)
 search.sort(key=lambda x: x[1], reverse=True)
 
-for i in search:
-    print("Content:" + i[0].page_content)
-    print("Relevance:" + str(i[1]))
-    print('------------------')
 
 
+
+st.title('🦜🔗 Semantic Search with OpenAI and Streamlit')
+# Create a text input box for the user
+if prompt:
+    # Then pass the prompt to the LLM
+    # response = agent_executor.run(prompt)
+    # ...and write it out to the screen
+    # st.write(response)
+
+    for i in search:
+        st.write("Content:" + i[0].page_content)
+        st.write("Relevance:" + str(i[1]))
+        st.write('------------------')
+
+    # # With a streamlit expander
+    # with st.expander('Document Similarity Search'):
+    #     # Find the relevant pages
+    #     search = store.similarity_search_with_score(prompt)
+    #     # Write out the first
+    #     st.write(search[0][0].page_content)
